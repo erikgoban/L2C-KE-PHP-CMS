9bf7f537065e0d6ab092f158a34e0070cdd52d67
Admin <admin@Admins-MacBook-Pro.local>
2018-05-15 12:41:15 +0200

    database l2c

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once dirname(__FILE__).'/../framework/helpers.php';
-$result = db_select("SELECT * FROM pages");
+$result = db_query("SELECT * FROM pages");
 
 foreach ($result as $page){
     echo $page->title."-".$page->content; 
